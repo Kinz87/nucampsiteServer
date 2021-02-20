09ddcf87125eb3d9c71c8a0734c54b1ea8057036
Joe Dickinson <Joedog13ish@hotmail.com>
2021-02-20 12:20:16 -0600

    Week 4 Workshop

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '450068583027612',
+        clientSecret: '3b6c4e0814bf3168d0e36799ddbe0d6d'
+    }
 }
